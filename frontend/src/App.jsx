87d088cb37025d31fea9fa57b947be76950f80f6
Rupesh Vishwakarma <116112162+rupeshh132@users.noreply.github.com>
2026-08-15 00:12:12 +0530

    feat: remove welcome splash popup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,6 @@ import { AuthProvider } from './context/AuthContext';
 import { ThemeProvider } from './context/ThemeContext';
 import AppRoutes from './routes/AppRoutes';
 import ScrollToTop from './components/layout/ScrollToTop';
-import WelcomeSplash from './components/common/WelcomeSplash';
 import GlobalErrorBoundary from './components/common/GlobalErrorBoundary';
 import { Toaster } from 'react-hot-toast';
 import './styles/global.css';
@@ -29,7 +28,6 @@ function App() {
         <ThemeProvider>
           <BrowserRouter>
             <ScrollToTop />
-            <WelcomeSplash />
             <GlobalErrorBoundary>
               <AppRoutes />
             </GlobalErrorBoundary>
